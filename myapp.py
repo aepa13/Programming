1c0d4694aadc4d16588e78268d67cbfb5663c6a7
Andrés Pocaterra <99044243+aepa13@users.noreply.github.com>
2022-04-20 20:59:21 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,1 +1,17 @@
-print('this is my app')
+import pandas as pd
+import numpy as np
+import streamlit as st
+import seaborn as sns
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+print(chart_data)
+
+st.title('This is my first web app')
+
+@st.cache
+
+st.dataframe(chart_data)
+
+option = st.selectbox('Select a column', chart_data.columns.tolist())
+sns.barplot(option)
+
