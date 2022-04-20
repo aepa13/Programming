171462ba580494b03b5d2612a4bcaa6f1f8e11b6
Andrés Pocaterra <99044243+aepa13@users.noreply.github.com>
2022-04-20 21:08:00 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-#import seaborn as sns
+import seaborn as sns
 
 st.title('This is my first web app')
 
@@ -10,4 +10,4 @@ chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.table(chart_data.iloc[0:])
 
 option = st.selectbox('Select a column', chart_data.columns.tolist())
-#print(sns.barplot(option))
+print(sns.barplot(option))
