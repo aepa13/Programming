f9d48f19c4074adfe29cf34a3e0548824bc8ab49
Andrés Pocaterra <99044243+aepa13@users.noreply.github.com>
2022-04-22 21:13:17 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -10,12 +10,6 @@ st.image(image)
 
 con = sqlite3.connect('excel_database.db') # Connect
 
-participants.to_sql('participants', con , if_exists='replace', index=True)
-projects.to_sql('projects', con , if_exists='replace', index=True)
-countries.to_sql('countries', con , if_exists='replace', index=True)
-
-database = 'ecsel_database.db'
-
 selects = {'participants':
     '''SELECT p.shortName, p.name, p.activityType, p.organizationURL, COUNT(*) as projects, SUM(p.ecContribution) as total_grants
         FROM  participants p, projects pr, countries c
