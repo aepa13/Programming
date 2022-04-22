762d3cf747885c6e9a3fa3d511b7b842193e06ab
Andrés Pocaterra <99044243+aepa13@users.noreply.github.com>
2022-04-22 21:19:47 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -18,7 +18,7 @@ selects = {'country': 'SELECT Acronym FROM countries',
         WHERE p.projectID = pr.projectID AND c.Acronym = p.country AND c.Country = '{}'
         GROUP BY p.name ORDER BY SUM(p.ecContribution) DESC'''}
 
-st.selectbox('Pick a European country',list(selects['country']))
+st.selectbox('Pick a European country',selects['country'])
 
 df = pd.read_sql(selects['participants'], con)
 st.dataframe(df)
