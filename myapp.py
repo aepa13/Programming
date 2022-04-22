0a506c5e75d9495e3f45cce889d5e2b7c76d07f4
Andrés Pocaterra <99044243+aepa13@users.noreply.github.com>
2022-04-22 21:30:05 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -16,8 +16,9 @@ selects = {'country': 'SELECT * FROM countries',
     '''SELECT p.shortName, p.name, p.activityType, p.organizationURL, COUNT(*) as projects, SUM(p.ecContribution) as total_grants
         FROM  participants p, projects pr, countries c
         WHERE p.projectID = pr.projectID AND c.Acronym = p.country AND c.Country = '{}'
-        GROUP BY p.name ORDER BY SUM(p.ecContribution) DESC'''
-    'grants':
+        GROUP BY p.name ORDER BY SUM(p.ecContribution) DESC''',
+    
+           'grants':
     '''SELECT SUM(o.ecContribution) AS grants
         FROM  organizations o JOIN project p ON o.projectID=p.projectID
         WHERE o.country = '{}'
